f6976e09bdeb0d97ca1e3f3ade88e8958d2bf5fe
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2023-07-07 17:43:35 +0200

    add components structure

---
+++ src/components/Categories.jsx
@@ -0,0 +1,12 @@
+import CategoriesMovieCard from "./CategoriesMovieCard";
+
+const Categories = () => {
+  return (
+    <div>
+      <h1>Categories</h1>
+      <CategoriesMovieCard />
+    </div>
+  );
+};
+
+export default Categories;
