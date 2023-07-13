be85e506f71df1620ff4a413480de9295129415b
claudia-sikorski <c.sikorski@outlook.de>
2023-07-13 19:54:51 +0200

    add header incl style elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,16 @@ import "./styles/header.css";
 const Header = () => {
   return (
     <div id="headerContainer">
-      <h1>Holy Film</h1>
+      <div id="headerTitle">
+        <img
+          src="holymovie.svg"
+          width="40"
+          height="40"
+          className="d-inline-block align-top"
+          alt="React Bootstrap logo"
+        />
+        <h1>Holy Film</h1>
+      </div>
       <p>Discover the best films here!</p>
     </div>
   );
